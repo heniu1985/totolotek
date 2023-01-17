dd4222552c7165cf4c63529b1ba364d61c98cde2
Damian Hennek <hennekd@gmail.com>
2023-01-17 01:02:20 +0100

    Pytest and Pylint - Install and program strucutre planning

---
+++ main.py
@@ -0,0 +1,30 @@
+'''Progam that calculates how long that will take win in Lotto lottery
+'''
+
+
+def welcome_message():
+    pass
+
+
+def type_of_numbers():
+    pass
+
+
+def take_user_numbers():
+    pass
+
+
+def draw_numbers():
+    pass
+
+
+def compare_numbers():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
